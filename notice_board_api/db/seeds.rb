2b1a6d57dc7bb1ba6513cd1dffdd3d4e79b26f34
Ric Mershon <rmersh@ricmershon.com>
2020-04-03 12:21:25 -0400

    Frontend API calls

--- notice_board_api/db/seeds.rb
+++ notice_board_api/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Wishlist.create([
+    {
+        user: 'Ric',
+        email:'example@example.com',
+        need_list: 'Toilet paper, paper towels, pizza, peace of mind',
+        have_list: 'Bottled water, cookies, coffee', where_to_find: "Ric's Bachelor Pad"
+    },
+    {
+        user: 'Vikram',
+        email: 'example2@example.com',
+        need_list: 'Toilet paper, dish soap, reasons to stay awake',
+        have_list: 'Gasoline, energy drinks, paper towels',
+        where_to_find: "Vikram's Bounce House"
+    }
+])
---
+++ notice_board_client/src/components/Main.js
@@ -0,0 +1,109 @@
+import React from 'react'
+
+// components
+import Wishlist from './Wishlist.js'
+import Form from './Form.js'
+
+// =============================
+// COMPONENT CLASS
+// =============================
+
+class Main extends React.Component {
+  state = {
+    wishlists: []
+  }
+
+  fetchWishlists = async () => {
+    let response = await fetch('http://localhost:3000/wishlists')
+    let data = await response.json()
+    console.log(data);
+    this.setState({ wishlists: data })
+  }
+
+  handleCreate = async (createData) => {
+    let response = await fetch('http://localhost:3000/wishlists',
+    {
+      body: JSON.stringify(createData),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    let data = await response.json()
+    this.props.handleView('home')
+    this.setState(prevState => {
+      return {
+        wishlists: [...prevState.wishlists, data]
+      }
+    })
+  }
+
+  handleUpdate = async (updateData) => {
+    let response = await fetch(`http://localhost:3000/wishlists${updateData.id}`,
+    {
+      body: JSON.stringify(updateData),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    let data = await response.json()
+    this.props.handleView('home')
+    this.fetchWishlists()
+  }
+
+  handleDelete = async (id) => {
+    let reponse = await fetch(`http://localhost:3000/wishlists/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    this.setState(prevState => {
+      const wishlists = prevState.wishlists.filter(wishlist => wishlist.id !== id)
+      return {
+        wishlists
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.fetchWishlists()
+  }
+
+
+
+  // ==============
+  // RENDER
+  // ==============
+  render () {
+    return (
+      <main>
+        <h1>{ this.props.view.pageTitle }</h1>
+        { this.props.view.page === 'home'
+          ? this.state.wishlists.map(wishlist => (
+              <Wishlist
+                key={wishlist.id}
+                wishlist={wishlist}
+                handleView={this.props.handleView}
+                handleDelete={this.handleDelete}
+              />
+            ))
+          : <Form
+              handleCreate={this.handleCreate}
+              handleUpdate={this.handleUpdate}
+              formInputs={this.props.formInputs}
+              view={this.props.view}
+            />
+        }
+      </main>
+    )
+  }
+}
+// =============================
+// EXPORT
+// =============================
+export default Main
